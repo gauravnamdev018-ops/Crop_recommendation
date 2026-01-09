bddbd21ed51132b37fa6bff13b5131dd3ad93969
gauravnamdev018-ops <gauravnamdev018@gmail.com>
2026-01-09 23:22:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,11 +144,4 @@ def recommend():
 # RUN SERVER
 # =============================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-@app.route("/")
-def home():
-    return "Crop Recommendation API is running"
-
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
